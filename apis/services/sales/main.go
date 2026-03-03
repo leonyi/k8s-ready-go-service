6e0b39dce83ae5b708d504f291fc3119ca7269a5
Yanet Leon <yanet.leon@gmail.com>
2026-03-03 12:53:12 -0800

    commit 2

---
+++ apis/services/sales/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/ardanlabs/service/foundation/logger"
+)
+
+func main() {
+	// constructs a pointer to the logger.
+	var log *logger.Logger
+
+	events := logger.Events{
+		// this should be done outside of this once we have a more ops environment. This is just to ensure we have something in place to test the events.
+		Error: func(ctx context.Context, r logger.Record) {
+			log.Info(ctx, "******* SEND ALERT *******")
+		},
+	}
+
+	traceIDFn := func(ctx context.Context) string {
+		return "" //web.GetTraceID(ctx)
+	}
+
+	// Construct the logger with a factory function
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SALES", traceIDFn, events)
+
+	// -------------------------------------------------------------------------
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "err", err)
+		os.Exit(1)
+	}
+
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+
+	// -------------------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// Creates a channle of type os.Signal with a buffer of 1 to receive shutdown signals.
+	// The signal.Notify function is used to register the channel to receive notifications for SIGINT and SIGTERM signals,
+	// which are common signals used to indicate that the program should terminate gracefully.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-shutdown
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
+}
